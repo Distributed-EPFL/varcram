0ae582a6b4ee40ad6b4c0062e491f9d4e93e0659
Matteo Monti <matteo.monti@epfl.ch>
2022-05-23 17:23:18 +0200

    Implement `encode`

---
+++ src/encode.rs
@@ -0,0 +1,24 @@
+use crate::bits;
+
+pub(crate) fn encode(values: &[u64]) -> (usize, Vec<u8>) {
+    let bits = bits(values);
+    let length = (values.len() * bits) / 8 + 8;
+
+    let mut buffer: Vec<u8> = vec![0; length];
+
+    for (index, value) in values.iter().copied().enumerate() {
+        let bit_offset = index * bits;
+        let byte_offset = bit_offset / 8;
+
+        let encoded = value << ((64 - bits) - (bit_offset - byte_offset * 8));
+        let encoded = encoded.to_be_bytes();
+
+        let window = &mut buffer.as_mut_slice()[byte_offset..(byte_offset + 8)];
+
+        for (window_byte, encoded_byte) in window.iter_mut().zip(encoded.iter()) {
+            *window_byte |= *encoded_byte;
+        }
+    }
+
+    (bits, buffer)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 // Modules
 
 mod bits;
+mod encode;
 
 // Functions
 
 use bits::bits;
+use encode::encode;
