29456941dba00f40e0c48e7d302bff81fc882f57
Matteo Monti <matteo.monti@epfl.ch>
2022-05-23 19:24:14 +0200

    Implement `VarCram`

--- src/varcram.rs
+++ src/varcram.rs
@@ -25,3 +25,45 @@ impl VarCram {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rand::prelude::*;
+
+    use std::iter;
+
+    fn invert(input: Vec<u64>) {
+        let cram = VarCram::cram(input.as_slice());
+        let output = cram.uncram().unwrap();
+
+        assert_eq!(input, output);
+    }
+
+    #[test]
+    fn manual() {
+        invert(vec![0]);
+        invert(vec![1]);
+        invert(vec![0, 1]);
+        invert(vec![0, 1, 0]);
+        invert(vec![4, 0, 1, 0]);
+        invert(vec![4, 0, 1, 0, 6]);
+        invert(vec![4, 0, 1, 0, 6, 1023]);
+        invert(vec![4, 0, 1, 0, 73, 6, 1023]);
+        invert(vec![4, 0, 65536, 1, 0, 6, 1023]);
+    }
+
+    #[test]
+    fn stochastic() {
+        for length in (1..1024).step_by(16) {
+            for bits in 0..16 {
+                let input = iter::repeat_with(|| random::<u64>() % (1 << bits))
+                    .take(length)
+                    .collect::<Vec<u64>>();
+
+                invert(input);
+            }
+        }
+    }
+}
